00c547610aa24ff1a8510e1740ab0badfe2499df
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-04-29 08:54:45 +0200

    push

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-HOST="192.168.54.195"
-USER="pi"
-
-GOOS=linux GOARCH=arm GOARM=7 go build -v -o ./bin/crashdetect-web main.go
-
-scp ./bin/crashdetect-web $USER@$HOST:/home/pi/Documents/crash_detector/crashdetect-web
+git add -A && git commit -m "push" && git push
